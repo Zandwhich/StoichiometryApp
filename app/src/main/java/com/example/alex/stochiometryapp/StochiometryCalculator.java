b8d3608f1240a7c9ecf4edb07212afa7564fe396
Zandwhich <alex.zdanowicz@gmail.com>
2017-08-29 22:08:20 -0400

    Continued to work on the Stochiometry Calculator.

--- app/src/main/java/com/example/alex/stochiometryapp/StochiometryCalculator.java
+++ app/src/main/java/com/example/alex/stochiometryapp/StochiometryCalculator.java
@@ -2,14 +2,39 @@ package com.example.alex.stochiometryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-import com.example.alex.stochiometryapp.R;
+import com.example.alex.util.Atom;
+import com.example.alex.util.Elements_OLD;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StochiometryCalculator extends AppCompatActivity {
+
+    Spinner fromSpinner;
+    Elements_OLD elements_OLD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stochiometry_calculator);
+
+        elements_OLD = new Elements_OLD();
+        fromSpinner = (Spinner) findViewById(R.id.from_spinner);
+
+        List<String> elementNames = new ArrayList<String>();
+
+        for (Atom atom : elements_OLD.allElements)
+        {
+            elementNames.add(atom.NAME);
+        }
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, elementNames);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fromSpinner.setAdapter(dataAdapter);
+
     }
 }
